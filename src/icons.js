f59200ad5fb06ffe09b2c9ca276bd201a1aa9f58
DoctorSpace <jackassdimas1@gmail.com>
2024-01-15 20:41:45 +0300

    refactor(search): del search bar

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,1 @@
 import './icons/heart.svg';
-import './icons/search.svg';
-import './icons/search_icon.svg';
-import './icons/search_button.svg';
--- src/modules/searchTips/searchTips.ts
+++ src/modules/searchTips/searchTips.ts
@@ -3,7 +3,7 @@ import { View } from '../../utils/view';
 import html from './searchTips.tpl.html';
 import { Component } from '../component';
 
-const SearchTipsName = ['чехол iphone 13 pro', 'коляски agex', 'яндекс станция 2']
+const SearchTipsName: string[] = ['чехол iphone 13 pro', 'коляски agex', 'яндекс станция 2'];
 
 class SearchTips extends Component {
   view: View;
@@ -13,16 +13,19 @@ class SearchTips extends Component {
     this.view = new ViewTemplate(html).cloneView();
   }
 
+  attach($root: HTMLElement) {
+    $root.innerHTML = '';
+    $root.appendChild(this.view.root);
+  }
 
-  private createElement = (tag: string, className: string, text?: string) =>{
+  private createElement = (tag: string, className: string, text?: string) => {
     const elem = document.createElement(tag);
     elem.className = className;
     if (text) elem.textContent = text;
     return elem;
-  }
+  };
 
-  addSearchTips(SearchTips: string[]){
-
+  addSearchTips(SearchTips: string[]): Element {
     const tipsContainer = this.view.root.querySelector('.search__examples');
 
     if (!tipsContainer) {
@@ -32,41 +35,32 @@ class SearchTips extends Component {
     tipsContainer.appendChild(this.createElement('span', 'search__normal-text', 'Например,'));
 
     SearchTips.forEach((SearchTip, index) => {
+      const tipExampleElem = this.createElement('div', 'search__example');
+      tipExampleElem.appendChild(this.createElement('span', 'search__example-text', SearchTip));
+      tipExampleElem.addEventListener('click', () => {
+        console.log('Запрос', SearchTip);
+      });
 
-        const tipExampleElem = this.createElement('div', 'search__example');
-        tipExampleElem.appendChild(this.createElement('span', 'search__example-text', SearchTip));
-        tipExampleElem.addEventListener('click', () => {
-            this.view.searchInput.value = SearchTip;
-        });
+      tipsContainer.appendChild(tipExampleElem);
 
-        tipsContainer.appendChild(tipExampleElem);
-
-        if (index == SearchTips.length - 1) {
-        } else if (index == SearchTips.length - 2) {
-          tipsContainer.appendChild(this.createElement('span', 'search__normal-text', 'или'));
-        } else {
-          tipsContainer.appendChild(this.createElement('span', 'search__normal-text_comma', ','));
-        }
-
+      if (index == SearchTips.length - 1) {
+      } else if (index == SearchTips.length - 2) {
+        tipsContainer.appendChild(this.createElement('span', 'search__normal-text', 'или'));
+      } else {
+        tipsContainer.appendChild(this.createElement('span', 'search__normal-text_comma', ','));
+      }
     });
 
     return tipsContainer;
   }
 
-
-
   async render() {
-    if (SearchTipsName.length == 0) {
-        this.view.root.classList.add('is__empty');
-        return
-    }
+    if (SearchTipsName.length == 0 && this.view.root.parentElement) {
+      this.view.root.parentElement.classList.add('hide');
+      return;
+    } 
 
     this.view.root.appendChild(this.addSearchTips(SearchTipsName));
-  }
-
-  attach($root: HTMLElement) {
-    $root.innerHTML = '';
-    $root.appendChild(this.view.root);
   }
 }
 
